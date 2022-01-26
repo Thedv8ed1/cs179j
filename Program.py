158895aff39a3089edc92ad21faafac7f73e4ca9
Xilinx User <thedv8ed1@hotmail.com>
2022-01-26 15:59:19 +0000

    Moved some logic from program into states

--- Program.py
+++ Program.py
@@ -5,13 +5,14 @@ import cv2
 import numpy as np
 from Enums import FilterState
 from Enums import Filter
-filters=Filter()
+from Buttons import BUTTONS
+
 
 #TODO come up with a better name than program
 class PROGRAM:
     def __init__(self):
         print("Starting program initialization")
-        self.base=BaseOverlay("base.bit")
+        self.base=BaseOverlay('base.bit')
         ## configure HDMI
         self.hdmi_in=self.base.video.hdmi_in
         self.hdmi_out=self.base.video.hdmi_out
@@ -19,65 +20,57 @@ class PROGRAM:
         self.hdmi_out.configure(self.hdmi_in.mode,PIXEL_RGB)
         self.hdmi_in.start()
         self.hdmi_out.start()
-        self.frame=None
-        self.is_running=True
+        ## create class data
+        self.in_frame=None ## holds frame data
+        self.is_running=True 
         self.state_machine=STATE_MACHINE(self)
+        self.filters=Filter() ## contains the currently set filter
+        self.button=BUTTONS(self.filters,self.base) ## contains funtionality for polling button input
         print("Finished initialization")
     def Update(self):
         self.state_machine.Update()
 
     def Is_Running(self):
         return self.is_running
+
     def Change_State(self,state):
         self.state_machine.Change_State(state)
 
     def Read_HDMI(self):
-        self.frame=self.hdmi_in.readframe()
+        self.in_frame=self.hdmi_in.readframe()
 
     def Write_HDMI(self):
-
-        filter=filters.getFilterState()
-
-        if (filter == FilterState.NONE):
-            self.frame = self.__applyNoFilter(self.frame)
-        elif (filter == FilterState.FILTER1):
-            self.frame = self.__applyGaussianBlur(self.frame)
-        elif (filter == FilterState.FILTER2):
-            self.frame = self.__applyLaplacian(self.frame)
-        elif (filter == FilterState.FILTER3):
-            self.frame = self.__applyNoFilter(self.frame) # TODO
-        elif (filter == FilterState.FILTER4):
-            self.frame = self.__applyNoFilter(self.frame) # TODO
-        else:
-            # default
-            self.frame = self.__applyNoFilter(self.frame)
-
-        self.hdmi_out.writeframe(self.frame)
+        self.hdmi_out.writeframe(self.in_frame)
 
     def Clean_Up(self):
         self.hdmi_out.close()
         self.hdmi_in.close()
 
+    def Poll_Input(self):
+        self.button.Poll_Buttons()
+
+    def Get_Filter(self):
+        return self.filters.getFilterState()
 
     # MARK: - Photo filters for HDMI input
+    def applyNoFilter(self):
+        return None # Dummy function
 
-    def __applyNoFilter(self, in_frame):
-        return in_frame # Dummy function
-
-    def __applyGaussianBlur(self, in_frame):
-        inter = self.hdmi_out.newframe()
+    def applyGaussianBlur(self):
+        outframe = self.hdmi_out.newframe()
         # Gaussian blur takes source, ksize, destination
-        cv2.GaussianBlur(in_frame, (15,15), 0, dst=inter)
-        return inter
+        cv2.GaussianBlur(self.in_frame, (15,15), 0, dst=outframe)
+        self.in_frame=outframe        
 
     # source notebook: https://github.com/Xilinx/PYNQ/blob/master/boards/Pynq-Z1/base/notebooks/video/hdmi_introduction.ipynb
-    def __applyLaplacian(self, in_frame):
+    def applyLaplacian(self):
         grayscale = np.ndarray(shape=(self.hdmi_in.mode.height, self.hdmi_in.mode.width),
-                       dtype=np.uint8)
+                            dtype=np.uint8)
         result = np.ndarray(shape=(self.hdmi_in.mode.height, self.hdmi_in.mode.width),
-                    dtype=np.uint8)
-        cv2.cvtColor(in_frame, cv2.COLOR_BGR2GRAY, dst=grayscale)
+                            dtype=np.uint8)
+        cv2.cvtColor(self.in_frame, cv2.COLOR_BGR2GRAY, dst=grayscale)
         cv2.Laplacian(grayscale, cv2.CV_8U, dst=result)
         outframe = self.hdmi_out.newframe()
         cv2.cvtColor(result, cv2.COLOR_GRAY2BGR,dst=outframe)
-        return outframe
+        self.in_frame=outframe 
+        
