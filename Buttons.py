f60faae83bfbd3f5e54753d9d10a69cf2ea1bff8
Xilinx User <thedv8ed1@hotmail.com>
2022-01-26 15:57:59 +0000

    Removed second instantiation of base overlay object, passed previous instantiation to button class

--- Buttons.py
+++ Buttons.py
@@ -1,55 +1,48 @@
 from Enums import FilterState
-from pynq.overlays.base import BaseOverlay
-base = BaseOverlay("base.bit")
 from Enums import Filter
-filter=Filter()
 
 
 class BUTTONS:
-
     # private methods
-    def __init__(self):
+    def __init__(self,filter,base):
         print("buttons initialized")
         # no filter applied by default
+        self.filter=filter
+        self.base=base
         self.__TurnOffAllLeds()
 
-    @classmethod
     def __TurnOffAllLeds(self):
-        for led in base.leds:
+        for led in self.base.leds:
             led.off()
 
-    @classmethod
     def __handleLEDForIndex(self, index: int):
         self.__TurnOffAllLeds()
-        base.leds[index].toggle() 
+        self.base.leds[index].toggle() 
 
     # public methods
     def Poll_Buttons(self):
         # check if any button has been pressed
 
         # button 1
-        if (base.buttons[0].read()==1):
+        if (self.base.buttons[0].read()==1):
             self.__handleLEDForIndex(0)
-            filter.setFilterState(FilterState.FILTER1)
+            self.filter.setFilterState(FilterState.FILTER1)
             print("selected filter for button 0")
     
         # button 2
-        elif (base.buttons[1].read()==1):
+        elif (self.base.buttons[1].read()==1):
             self.__handleLEDForIndex(1)
-            filter.setFilterState(FilterState.FILTER2)
+            self.filter.setFilterState(FilterState.FILTER2)
             print("selected filter for button 1")
 
         # button 3
-        elif (base.buttons[2].read()==1):
+        elif (self.base.buttons[2].read()==1):
             self.__handleLEDForIndex(2)
-            filter.setFilterState(FilterState.FILTER3)
+            self.filter.setFilterState(FilterState.FILTER3)
             print("selected filter for button 2")
 
         # button 4
-        elif (base.buttons[3].read()==1):
+        elif (self.base.buttons[3].read()==1):
             self.__handleLEDForIndex(3)
-            filter.setFilterState(FilterState.FILTER4)
+            self.filter.setFilterState(FilterState.FILTER4)
             print("selected filter for button 3")
-
-        
-
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import traceback
 from Program import PROGRAM
-from Buttons import BUTTONS
 program=PROGRAM()
-buttons=BUTTONS()
+
 try:
     while program.Is_Running():
-        program.Update()
-        buttons.Poll_Buttons()
+        program.Update()   
 except (Exception) as e:
     traceback.print_exc()
     print("Error type: ", e.__class__.__name__)
